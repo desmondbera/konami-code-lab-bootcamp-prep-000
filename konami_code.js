176c08f502aa4b20c467e5f8c6b468a373153d37
desmond beramendi <desmond7187@gmail.com>
2018-01-09 03:34:43 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,19 +1,8 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
-  var indx = 0;
-  document.body.addEventListener("keydown",function(event){
-    const key = parseInt(event.which || event.detail);
-    if (key === code[indx]) {
-      indx++;
-      if (indx === code.length) {
-        alert("Hurray!");
-        indx = 0;
-      }
-    }else {
-      indx = 0 ;
-    }
-  });
+var indx = 0; 
+
+
 
 }
-console.log(e.detail);
