0d9827fe69684534701c3040915464bc75deee54
desmond beramendi <desmond7187@gmail.com>
2018-01-09 03:41:25 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,11 +3,11 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 function init() {
 var indx = 0; 
 document.body.addEventListener('keydown', function(evnt) {
-  const key = (evnt.which || evnt.detail)
+  const key = parseInt(evnt.which || evnt.detail)
   if(code[indx] === key) {
     indx++
     if(indx === code.length) {
-      alert("Hurray you got it!")
+      alert("Hurray MOTHERFUCKER!")
       indx = 0 
     }
   } else {
