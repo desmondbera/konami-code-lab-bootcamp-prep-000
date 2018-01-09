78c4c9e6582e29bdaf02ee9d95b8130257c70f85
desmond beramendi <desmond7187@gmail.com>
2018-01-09 03:39:47 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -6,7 +6,7 @@ document.body.addEventListener('keydown', function(evnt) {
   const key = (evnt.which || evnt.detail)
   if(code[indx] === key) {
     indx++
-    if(indx === key.length) {
+    if(indx === code.length) {
       alert("Hurray you got it!")
       indx = 0 
     }
