e7923f8ddeca717ee41ee42566c1c240e52a18cb
desmond beramendi <desmond7187@gmail.com>
2018-01-09 03:21:56 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,8 +2,8 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   var indx = 0 
-  document.body.addEventListener("keydown",function(e){
-  
+  document.body.addEventListener("keydown",function(event){
+    
   
     
   })
